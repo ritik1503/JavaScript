66a8ed0b63f67bd66c216939ac2309f5c60450ec
Ritik Verma <ritikz1234@gmail.com>
2024-10-16 09:33:02 +0530

    learned Dialog boxes

---
+++ 3 - Dialog Boxes - Alert , Confirm & Prompt/script.js
@@ -0,0 +1,6 @@
+alert("Hello, Alert is working perfect")
+let isConfirmClicked = confirm("Are you sure ?")
+console.log(isConfirmClicked);
+
+let getValueFromPrompt = prompt("Enter your details")
+console.log(getValueFromPrompt);
