4a18974435807a2f7206fd5050aced481347c35b
Ritik Verma <ritikz1234@gmail.com>
2024-10-17 10:19:41 +0530

    learned Math and truthy-falsy

---
+++ 5 - Math/script.js
@@ -0,0 +1,8 @@
+console.log(Math.PI);
+console.log(Math.sqrt(16));
+console.log(Math.floor(5.9999)); //it will remove decimal values only and doesnot round off the given number
+console.log(Math.ceil(11.23)); //remove decimal and make it greater than 1(for positive numbers)
+console.log(Math.round(2.499999999999999)); //if less than 2.5 then make it 2 else 3
+console.log(Math.random()); //Returns a pseudorandom number between 0 and 1.(but never 1)
+console.log(isNaN('a12')); //return true if it contains number with alphabets, special characters
+
