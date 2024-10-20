9aa26a18b24ad0b8c8f4b0dba539f6fddbbe9e63
Ritik Verma <ritikz1234@gmail.com>
2024-10-20 22:08:00 +0530

    learned objects

---
+++ 6 - Object/script.js
@@ -0,0 +1,10 @@
+const name = 'Ritik';
+
+const user = { firstName: "Ritik", lastName: "Verma",Ritik:'hello', address: {city:'gorakhpur',state:'Uttar Pradesh'} };
+console.log(user.firstName);
+console.log(user.lastName);
+console.log(user.address.city);
+console.log(user.address.state);
+
+console.log(user["firstName"]);
+console.log(user[name]);
